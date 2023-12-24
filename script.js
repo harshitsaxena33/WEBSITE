e7c11a563207156c63cead696234a10aeb863c18
HARSHIT SAXENA <139982376+harshitsaxena33@users.noreply.github.com>
2023-12-24 18:15:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-const dynamicText = document.querySelector("h2 span");
-const words = ["HARSHIT SAXENA", "Coder", "Front End Developer", "UI/UX Designer","Tech Enthusiast"];
+// JavaScript to trigger the typing animation
+const text = "I'M HARSHIT SAXENA";
+let index = 0;
 
-
-let wordIndex = 0;
-let charIndex = 0;
-let isDeleting = false;
-
-const typeEffect = () => {
-    const currentWord = words[wordIndex];
-    const currentChar = currentWord.substring(0, charIndex);
-    dynamicText.textContent = currentChar;
-    dynamicText.classList.add("stop-blinking");
-
-    if (!isDeleting && charIndex < currentWord.length) {
-        
-        charIndex++;
-        setTimeout(typeEffect, 200);
-    } else if (isDeleting && charIndex > 0) {
-        
-        charIndex--;
-        setTimeout(typeEffect, 100);
-    } else {
-        
-        isDeleting = !isDeleting;
-        dynamicText.classList.remove("stop-blinking");
-        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
-        setTimeout(typeEffect, 1200);
-    }
+function type() {
+  if (index < text.length) {
+    document.getElementById('typedText').innerText += text.charAt(index);
+    index++;
+    setTimeout(type, 150); // Adjust typing speed here (in milliseconds)
+  }
 }
 
-typeEffect();
+// Start the typing animation when the window loads
+window.onload = function() {
+  type();
+};
